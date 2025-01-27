c06f35867d28353fe8bffbdd9a78e07e3430d900
Robert Ellis <mr.bob.ellis@gmail.com>
2025-01-27 18:04:58 +0000

    h2 and ul .css file changes

--- script/myScript.js
+++ script/myScript.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
+    $("h2").css("text-decoration", "underline");//underlines all h2 elements
+    $("ul").css("Border", ("solid 1px #ccc"));//adds a border to the list
     
 });
