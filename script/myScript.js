3ccef80cafa6d722519eac88b093bc877dbaa5bb
Robert Ellis <mr.bob.ellis@gmail.com>
2025-01-30 20:21:26 +0000

    File transfers and file structure setup

--- script/myScript.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("h2").css("text-decoration", "underline");//underlines all h2 elements
-    $("ul").css("Border", ("solid 1px #ccc"));//adds a border to the list
-    
-});
