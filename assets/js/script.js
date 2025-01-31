a5c2b71be8c7ec870a4edc5ceb6828f94ce1b993
Robert Ellis <mr.bob.ellis@gmail.com>
2025-01-31 21:19:14 +0000

    modal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 const API_KEY = "vfJ7S26GVQTDLagHoEQ1EJUndss";
-const API_URL = "https://ci-jshint.herokusapp.com/api/";
-const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
